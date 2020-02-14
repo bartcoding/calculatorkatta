2ea3d1c20c16e92faea3dc793a03eb5b58440cc3
bartAtHome <hunerbeinbart@gmail.com>
2020-02-14 23:16:38 +0100

    Very first (bad) implementation of calculator Super simple and works for the given test cases all tests pass

--- src/main/java/be/intecbrussel/testing/calculator/StringCalculator.java
+++ src/main/java/be/intecbrussel/testing/calculator/StringCalculator.java
@@ -3,9 +3,21 @@ package be.intecbrussel.testing.calculator;
 public class StringCalculator {
 
     public int add(String numbers){
+        int number1=0;
+        int number2=0;
+        int number3=0;
+        if (numbers.equals("")){
+            return 0;
+        }
+        if(numbers.split(",").length==1){
+            number2=Integer.parseInt(numbers.split(",")[0]);
+        }
+        if(numbers.split(",").length==2){
+            number2=Integer.parseInt(numbers.split(",")[0]);
+            number3=Integer.parseInt(numbers.split(",")[1]);
+        }
 
-
-        return 0;
+        return number1+number2+number3;
     }
 
 
