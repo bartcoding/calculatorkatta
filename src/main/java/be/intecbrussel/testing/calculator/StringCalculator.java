362841aac9c2b8909aa0cabde58f9799f1237cdf
bartAtHome <hunerbeinbart@gmail.com>
2020-02-15 20:41:22 +0100

    implement multiple delimiter character possibility tests GREEN

--- src/main/java/be/intecbrussel/testing/calculator/StringCalculator.java
+++ src/main/java/be/intecbrussel/testing/calculator/StringCalculator.java
@@ -9,34 +9,35 @@ import java.util.stream.Stream;
 
 public class StringCalculator {
 
-    private int count =0;
+    private int count = 0;
 
-    public int add(String numbers){
+    public int add(String numbers) {
         count++;
         if (numbers.equals("")) {
             return 0;
         }
-        int[] numberArray =convertStringOfNumbersToIntArray(numbers);
+        int[] numberArray = convertStringOfNumbersToIntArray(numbers);
         checkForNegatives(numberArray);
         return IntStream.of(numberArray)
-                .filter(number->number<1000)
+                .filter(number -> number < 1000)
                 .sum();
     }
-    public int getCalledCount(){
+
+    public int getCalledCount() {
         return count;
     }
 
     private void checkForNegatives(int[] numberArray) {
         List<Integer> negativeNumbers = new ArrayList<>();
-        for (int number :numberArray){
-            if (number<0){
+        for (int number : numberArray) {
+            if (number < 0) {
                 negativeNumbers.add(number);
             }
         }
-        if (negativeNumbers.size()>0){
+        if (negativeNumbers.size() > 0) {
             String negativeNumbersAsStringWithCommas = negativeNumbers.stream()
-                                                                        .map(Objects::toString)
-                                                                        .collect(Collectors.joining(","));
+                    .map(Objects::toString)
+                    .collect(Collectors.joining(","));
             throw new RuntimeException("negatives not allowed : " + negativeNumbersAsStringWithCommas);
         }
     }
@@ -50,13 +51,21 @@ public class StringCalculator {
         String delimiterCharacters = "[,\n]";
         if (stringOfNumbers.startsWith("//")) {
             delimiterCharacters = getDelimiterCharachters(stringOfNumbers);
-            stringOfNumbers = stringOfNumbers.substring(4);
+            if (stringOfNumbers.startsWith("//[")) {
+                stringOfNumbers = stringOfNumbers.substring(stringOfNumbers.indexOf(']') + 2);
+            } else {
+                stringOfNumbers = stringOfNumbers.substring(4);
+            }
         }
-        return stringOfNumbers.split(delimiterCharacters.toString());
+        return stringOfNumbers.split(delimiterCharacters);
     }
 
     private String getDelimiterCharachters(String stringOfNumbers) {
-        return "["+stringOfNumbers.charAt(2)+",\n]";
+        if (stringOfNumbers.charAt(2) == '[') {
+            int endIndex = stringOfNumbers.indexOf(']');
+            return "[,\n]|" + stringOfNumbers.substring(3, endIndex);
+        }
+        return "[" + stringOfNumbers.charAt(2) + ",\n]";
     }
 
 
