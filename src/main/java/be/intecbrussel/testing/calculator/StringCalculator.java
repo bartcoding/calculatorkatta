17509b4cf8bfd6423fa5252f2a707bf6dde1acc6
bartAtHome <hunerbeinbart@gmail.com>
2020-02-15 17:56:45 +0100

    Refactored code, tests still checkout

--- src/main/java/be/intecbrussel/testing/calculator/StringCalculator.java
+++ src/main/java/be/intecbrussel/testing/calculator/StringCalculator.java
@@ -1,23 +1,25 @@
 package be.intecbrussel.testing.calculator;
 
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class StringCalculator {
 
     public int add(String numbers){
-        int number1=0;
-        int number2=0;
-        int number3=0;
-        if (numbers.equals("")){
+        if(numbers.equals("")){
             return 0;
         }
-        if(numbers.split(",").length==1){
-            number2=Integer.parseInt(numbers.split(",")[0]);
-        }
-        if(numbers.split(",").length==2){
-            number2=Integer.parseInt(numbers.split(",")[0]);
-            number3=Integer.parseInt(numbers.split(",")[1]);
-        }
+        return IntStream.of(convertStringOfNumbersToIntArray(numbers)).sum();
+    }
 
-        return number1+number2+number3;
+    private int[] convertStringOfNumbersToIntArray(String stringOfNumbers){
+        String[] stringArrayOfNumbers = stringOfNumbers.split(",");
+        return convertStringOfNumberArrayToIntArray(stringArrayOfNumbers);
+    }
+
+    private int[] convertStringOfNumberArrayToIntArray(String[] stringOfNumbers) {
+        return Stream.of(stringOfNumbers).mapToInt(Integer::parseInt)
+                                         .toArray();
     }
 
 
