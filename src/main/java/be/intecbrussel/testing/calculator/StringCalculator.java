bd1857aa938efbec36b8b62d31068b54e574cef6
bartAtHome <hunerbeinbart@gmail.com>
2020-02-15 19:10:07 +0100

    refactor StringCalculator code tests still GREEN

--- src/main/java/be/intecbrussel/testing/calculator/StringCalculator.java
+++ src/main/java/be/intecbrussel/testing/calculator/StringCalculator.java
@@ -1,34 +1,32 @@
 package be.intecbrussel.testing.calculator;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class StringCalculator {
 
-    public int add(String numbers){
-        if(numbers.equals("")){
+    public int add(String numbers) {
+        if (numbers.equals("")) {
             return 0;
         }
         return IntStream.of(convertStringOfNumbersToIntArray(numbers)).sum();
     }
 
-    private int[] convertStringOfNumbersToIntArray(String stringOfNumbers){
-        String[] stringArrayOfNumbers = stringOfNumbers.split(",");
-        List<String> numberAsStringList = new ArrayList<>();
-        for (String s: stringArrayOfNumbers){
-            for(String number :s.split("\n")){
-                numberAsStringList.add(number);
-            }
-        }
-        stringArrayOfNumbers = numberAsStringList.toArray(new String[numberAsStringList.size()]);
+    private int[] convertStringOfNumbersToIntArray(String stringOfNumbers) {
+        String[] stringArrayOfNumbers = extractNumbersFromString(stringOfNumbers);
         return convertStringOfNumberArrayToIntArray(stringArrayOfNumbers);
+    }
+
+    private String[] extractNumbersFromString(String stringOfNumbers) {
+        return stringOfNumbers.split("[,\n]");
     }
 
     private int[] convertStringOfNumberArrayToIntArray(String[] stringOfNumbers) {
         return Stream.of(stringOfNumbers).mapToInt(Integer::parseInt)
-                                         .toArray();
+                .toArray();
     }
 
 
